d958187b7e1500cf7dd11df9c71d9bde34a91e0e
Zan Spiler <zan.spiler@celtra.com>
2024-10-06 20:11:52 +0200

    Add loader

--- components/Carousel.vue
+++ components/Carousel.vue
@@ -5,16 +5,21 @@
                 <div v-for="(image, index) in images" class="embla-slide">
                     <img 
                         v-if="imagesToFetch.includes(index)"
+                        v-show="fetchedImages.includes(index)"
                         :src="`/images/resized/${album}/${getResizedImageFilename(image, 1024)}`" 
                         :srcset="`
                         /images/resized/${album}/${getResizedImageFilename(image, 640)} 640w, 
                         /images/resized/${album}/${getResizedImageFilename(image, 1024)} 1024w, 
                         `" 
+                        @load="onImgLoad(index)"
                         sizes="70vw"
                         :alt="`Image ${index}`" 
                     />
+                    
+                    <Loader class="loader" v-if="!fetchedImages.includes(index)" />
+                    
                     <!-- NOTE: sizes not optimal but this is extremely boring -->
-                    <label class="counter">{{ index + 1 }} / {{ images.length }}</label>
+                    <label v-show="fetchedImages.includes(index)" class="counter">{{ index + 1 }} / {{ images.length }}</label>
                 </div>
             </div>
         </div>
@@ -33,8 +38,12 @@ const props = defineProps<{
 const [emblaRef, emblaApi] = emblaCarouselVue({duration: 0})
 
 const images = (imageList as Record<string, string[]>)[props.album] ?? [];
-
 const imagesToFetch = ref(props.initialSlide ? [props.initialSlide] : [0])
+const fetchedImages: Ref<number[]> = ref([])
+
+function onImgLoad(index: number) {
+    fetchedImages.value.push(index)
+}
 
 function logSlidesInView(emblaApi) {
     prefetchImages(emblaApi.slidesInView())
@@ -60,7 +69,6 @@ onMounted(() => {
     if (emblaApi.value) {
         emblaApi.value.on('slidesInView', logSlidesInView)
         if (props.initialSlide) {
-
             // NOTE hack to open carousel at Nth slide
             emblaApi.value.scrollTo(props.initialSlide)
             emblaApi.value.reInit({duration: 25}) 
@@ -108,9 +116,12 @@ img {
     left: 0;
     max-width: 100%;
     height: 100%;
-    // max-height: 100%;
     object-fit: cover;
     user-select: none;
+}
+
+.loader {
+    position: absolute;
 }
 
 .counter {
---
+++ components/Loader.vue
@@ -0,0 +1,55 @@
+<template>
+    <div class="cube">
+        <div class="face front"></div>
+        <div class="face back"></div>
+        <div class="face right"></div>
+        <div class="face botom"></div>
+    </div>
+</template>
+
+<style scoped lang="less">
+@cube-size: 50px;
+.cube {
+    width: @cube-size;
+    height: @cube-size;
+    transform-style: preserve-3d;
+    animation: rotateAnimation 6s linear infinite;
+}
+
+@keyframes rotateAnimation {
+    0% {
+        transform: rotateX(0deg) rotateY(0deg);
+    }
+    100% {
+        transform: rotateX(360deg) rotateY(360deg);
+    }
+}
+
+.face {
+    position: absolute;
+    width: @cube-size;
+    height: @cube-size;
+    background: red;
+    opacity: 0.5;
+}
+
+.front {
+    background: red;
+    transform: rotateY(0deg) translateZ(calc(@cube-size / 2));
+}
+
+.back {
+    background: blue;
+    transform: rotateY(180deg) translateZ(calc(@cube-size / 2));
+}
+
+.right {
+    background: green;
+    transform: rotateY(90deg) translateZ(calc(@cube-size / 2));
+}
+
+.botom {
+    background: rgb(255, 255, 161);
+    transform: rotateX(90deg) translateZ(calc(@cube-size / 2));
+}
+</style>
